d50d0fdef02390925f68a6d4c5104c48e5183065
Mohammad <m.verdizadeh@hotmail.com>
2019-06-16 17:36:35 -0400

    Adding controllers

---
+++ Controllers/pagesController.js
@@ -0,0 +1,23 @@
+const ToDo = require('../models/ToDo');
+
+exports.index =(req.res) => {
+
+}
+exports.show =(req.res) => {
+    
+}
+exports.new =(req.res) => {
+    
+}
+exports.update =(req.res) => {
+    
+}
+exports.create =(req.res) => {
+    
+}
+exports.edit =(req.res) => {
+    
+}
+exports.destroy =(req.res) => {
+    
+}
