7da020d9986ee5c010845732e0342dc7f4aadf57
Lalit <lalit255290@gmail.com>
2019-06-27 16:40:39 -0400

    deal

--- Controllers/TasksController.js
+++ Controllers/TasksController.js
@@ -1,19 +1,30 @@
 const Task = require('../models/task');
 
 exports.index = (req, res) => {
-    Task.find()
-    .then(tasks => {
-        res.render('tasks/index', {
-            tasks: tasks,
-            title: 'To do List'
+    req.isAuthenticated();
+
+    Task.find({
+        author: req.session.userId
+      })
+        .populate('author')
+        .then(tasks => {
+            res.render('tasks/index', {
+                tasks: tasks,
+                title: 'To do List'
         });
     })
-    .catch(err => {
-        console.error(`Error: ${err}`);
+        .catch(err => {
+            req.flash('error', `ERROR: ${err}`);
+            res.redirect('/');
     });
 };
 exports.show = (req, res) => {
-    Task.findById(req.params.id)
+    req.isAuthenticated();
+
+    Task.findOne({
+        _id: req.params.id,
+        author: req.session.userId
+    })
     .then(task => {
         res.render('tasks/show',{
             task: task,
@@ -21,44 +32,57 @@ exports.show = (req, res) => {
         });
     })
     .catch(err => {
-        console.error(`Error: ${err}`);
+        req.flash('error', `ERROR: ${err}`);
+        res.redirect('/');
     });
 };
 exports.new =(req, res) => {
+    req.isAuthenticated();
+
     res.render('tasks/new', {
         title: 'New Task Post'
     });
 }
 exports.update =(req, res) => {
+    req.isAuthenticated();
+
     Task.updateOne({
-        _id: req.body.id
+        _id: req.body.id,
+        author: req.session.userId
     },req.body.task, {
         runValidators: true
     })
     .then(()=> {
+        req.flash('success', 'The List was updated.');
         res.redirect(`/tasks/${req.body.id}`);
     })
     .catch(err => {
-        console.error(`ERROR: ${err}`);
+        req.flash('error', `ERROR: ${err}`);
+        res.redirect(`/tasks/${req.body.id}/edit`);
     });
 };
 
-exports.new = ( req,res ) => {
-    res.render('tasks/new', {
-        title: 'New Tasks Post'
-    });
-}
 exports.create =(req, res) => {
+    req.isAuthenticated();
+
+    req.body.task.author = req.session.userId;
     Task.create(req.body.task)
     .then(() => {
-       res.redirect('/tasks');
+        req.flash('success', 'New List was created successfully.');
+        res.redirect('/tasks');
     })
     .catch(err => {
-        console.error(`Error: ${err}`);
+        req.flash('error', `ERROR: ${err}`);
+        res.redirect('/tasks/new')
     });
 }
 exports.edit =(req, res) => {
-    Task.findById(req.params.id)
+    req.isAuthenticated();
+
+    Task.findOne({
+        _id: req.params.id,
+        author: req.session.userId
+    })
     .then(task => {
         res.render('tasks/edit',{
             task: task,
@@ -66,17 +90,23 @@ exports.edit =(req, res) => {
         });
     })
     .catch(err => {
-        console.error(`Error: ${err}`);
+        req.flash('error', `ERROR: ${err}`);
+        res.redirect('/');
     });
 }
 exports.destroy =(req, res) => {
+    req.isAuthenticated();
+
     Task.deleteOne({
-        _id: req.body.id
+        _id: req.body.id,
+        author: req.session.userId
     })
     .then(() => {
-        res.redirect('/tasks');
+        req.flash('success', 'The List was deleted.');
+        res.redirect('/tasks')
     })
     .catch(err => {
-        console.error(`ERROR: ${err}`);
+        req.flash('error', `ERROR: ${err}`);
+        res.redirect(`/tasks`);
     });
 }
