ee05a25459665f53c769de4db673548f1b434ea2
Noor Chaudhry <noor.m.chaudhry@gmail.com>
2019-08-04 17:07:43 -0400

    Update About section
    Add Noor, Moe and Steven's section

--- client/src/components/pages/about.js
+++ client/src/components/pages/about.js
@@ -7,8 +7,8 @@ function About () {
         <br/>
         <h1> About the team </h1>
         <p>Lalit: In the fourth Semester of Computer Programming. I am 20 years old. I am Indian</p>
-        <p></p>
-        <p></p>
+        <p>Noor: 22 years old, getting married August 10th, aspiring project manager and lifelong learner. </p>
+        <p>Moe: </p>
         <p></p>
         </header>
         )
