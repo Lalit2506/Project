c1b031a833adf7963fe2a2db5cf2834b6a20cdce
Lalit <lalit255290@gmail.com>
2019-08-11 10:31:44 -0400

    Login and logout message

--- client/src/components/sessions/logout.js
+++ client/src/components/sessions/logout.js
@@ -1,10 +1,28 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useContext} from "react";
 import Axios from "axios";
 import {Redirect} from "react-router-dom";
+import NotificationContext from "../notification_context";
 
 function Logout() {
+
+    const {setNotification} = useContext(NotificationContext);
+
     useEffect(() => {
-        Axios.post("/api/logout");
+        Axios.post("/api/logout")
+    .then(() => setNotification(notification => {
+      return {
+        ...notification,
+        status: "success",
+        message: "Logged out"
+      };
+    }))
+    .catch(() => setNotification(notification => {
+      return {
+        ...notification,
+        status: "danger",
+        message: "you are not going anywhere"
+      };
+    }));
     }, []);
 
     return <Redirect to="/" />
